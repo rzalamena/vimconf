5b7951aec1040d58f2fcc8bbc7e8d9d78cef6787
Rafael Zalamena <rzalamena@gmail.com>
2021-11-05 10:03:32 -0300

    lsp: remove plugin and configuration
    It is still not good enough even after all this time.

--- pack/plugins/start/lsp-settings/installer/install-rust-analyzer.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-setlocal
-curl -L -o "rust-analyzer-windows.exe" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-windows.exe"
-
-move rust-analyzer-windows.exe rust-analyzer.exe
